caf5690688d69951a180d44a98b271900a0991c8
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-27 12:56:53 +0000

    redux store setup done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import './index.css';
 import App from './Components/App';
 import movies from './reducer';
 const store = createStore(movies)
+store.dispatch({
+  type:'MOVIE_ADD',
+  movies:[{name:'Spiderman'}]
+})
 console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
