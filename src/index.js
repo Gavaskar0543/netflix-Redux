d7627088525109589ae080a2631f93202a6dd481
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-08-02 02:33:10 +0000

    created middleware using carried function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {createStore} from 'redux';
+import {createStore , applyMiddleware} from 'redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import combineReducers from './reducer';
-const store = createStore(combineReducers)
+// const logger = function({dispatch,getState}){
+//   return function(next){
+//     return function(action){
+//       console.log('Action_Type =' , action.type);
+//       next(action);
+//     }
+//   }
+// }
+const logger = ({dispatch,getState}) => (next) => (action) =>{
+  console.log('Action_TYPE =', action.type);
+  next(action);
+}
+const store = createStore(combineReducers , applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
